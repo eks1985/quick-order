8e8af259c5cb4b4cc730ea0ac837a05ef55348cb
Efimov Kirill <ya.kefimov@yandex.ru>
2016-12-05 13:02:17 +0000

    save local changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,7 +3,7 @@ export default {
 		apiKey: "AIzaSyBzPxu9Yt_kPLReqaN3J3tpLJK3OJky_gI",
     authDomain: "quick-order-de84c.firebaseapp.com",
     databaseURL: "https://quick-order-de84c.firebaseio.com",
-    storageBucket: "quick-order-de84c.appspot.com",
-    messagingSenderId: "975539781424"
+    // storageBucket: "quick-order-de84c.appspot.com",
+    // messagingSenderId: "975539781424"
 	}
 };
