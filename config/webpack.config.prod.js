52b5bf42f9d0840abe24fcfe1c8c837f76b988c4
Efimov Kirill <eks1985@yandex.ru>
2017-01-16 23:08:18 +0300

    rebuild site

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -65,7 +65,7 @@ module.exports = {
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
     // filename: 'static/js/[name].[chunkhash:8].js',
-    filename: 'static/js/[name].js',
+    filename: 'static/js/[name].[chunkhash:8].js',
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath
