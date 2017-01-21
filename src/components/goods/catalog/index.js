cde5f5618a944ab97576a3e6ad9cadc4b61fa175
Efimov Kirill <eks1985@yandex.ru>
2017-01-21 22:39:41 +0300

    rebuild site

--- src/components/goods/catalog/index.js
+++ src/components/goods/catalog/index.js
@@ -8,23 +8,35 @@ import { setModal } from './../../../lib/modal/actions/modal';
 // import img1 from './../../../image.jpg';
 import {List, ListItem} from 'material-ui/List';
 
-import img1 from './../../../images/178261540_8e2cd0d4e0_o.jpg';
-import img2 from './../../../images/2381371105_ee7ed440b5_o.jpg';
-import img3 from './../../../images/2398557289_2f7a6b311a_z.jpg';
-import img4 from './../../../images/5047875348_6b5e643b3f_z.jpg';
-import img5 from './../../../images/5822468112_1d29aa1088_z.jpg';
-import img8 from './../../../images/5873149343_3a15fc199e_z.jpg';
-import img6 from './../../../images/6739532325_a5521f83ee_z.jpg';
-import img7 from './../../../images/7308703522_389381bbcf_z.jpg';
-import img9 from './../../../images/8324712083_c7feeb2318_z.jpg';
-import img10 from './../../../images/8922827265_af56de2203_z.jpg';
-import img11 from './../../../images/13086218003_c39edd349e_z.jpg';
-import img12 from './../../../images/13957648459_dc3aee8b8c_z.jpg';
-import img13 from './../../../images/14690349697_22e243679e_z.jpg';
-import img14 from './../../../images/15263695669_1a3b3ea2f7_z.jpg';
-import img15 from './../../../images/15590414733_2bd4428b49_o.jpg';
+// import img1 from './../../../images/178261540_8e2cd0d4e0_o.jpg';
+// import img2 from './../../../images/2381371105_ee7ed440b5_o.jpg';
+// import img3 from './../../../images/2398557289_2f7a6b311a_z.jpg';
+// import img4 from './../../../images/5047875348_6b5e643b3f_z.jpg';
+// import img5 from './../../../images/5822468112_1d29aa1088_z.jpg';
+// import img8 from './../../../images/5873149343_3a15fc199e_z.jpg';
+// import img6 from './../../../images/6739532325_a5521f83ee_z.jpg';
+// import img7 from './../../../images/7308703522_389381bbcf_z.jpg';
+// import img9 from './../../../images/8324712083_c7feeb2318_z.jpg';
+// import img10 from './../../../images/8922827265_af56de2203_z.jpg';
+// import img11 from './../../../images/13086218003_c39edd349e_z.jpg';
+// import img12 from './../../../images/13957648459_dc3aee8b8c_z.jpg';
+// import img13 from './../../../images/14690349697_22e243679e_z.jpg';
+// import img14 from './../../../images/15263695669_1a3b3ea2f7_z.jpg';
+// import img15 from './../../../images/15590414733_2bd4428b49_o.jpg';
 
-const arr = [ img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15 ];
+const img1 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F13086218003_c39edd349e_z.jpg?alt=media&token=868bb547-42b6-46ae-9234-641adf6ad26b';
+const img2 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F13957648459_dc3aee8b8c_z.jpg?alt=media&token=84fc3f08-95c0-4422-bdf7-e6300ec7e41e';
+const img3 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F14690349697_22e243679e_z.jpg?alt=media&token=38f2c24f-0b34-4035-8e26-9a410731850a';
+const img4 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F15263695669_1a3b3ea2f7_z.jpg?alt=media&token=7d525240-2133-40e5-9b49-e3c2765aa789';
+const img5 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F15590414733_2bd4428b49_o.jpg?alt=media&token=e9d9d05e-d9f8-4f81-887e-96fd5c11231c';
+const img6 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F178261540_8e2cd0d4e0_o.jpg?alt=media&token=62162a8c-34ce-46a3-8295-cfd3794a2e53';
+const img7 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F5047875348_6b5e643b3f_z.jpg?alt=media&token=4a895bc6-0132-4882-ac55-922ea12f13fe';
+const img8 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F5822468112_1d29aa1088_z.jpg?alt=media&token=92b733fa-b232-495c-afdd-0d64d7a2752d';
+const img9 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F5873149343_3a15fc199e_z.jpg?alt=media&token=f97c6eb6-3902-4acf-9886-c70bf85ad8bf';
+const img10 = 'https://firebasestorage.googleapis.com/v0/b/quick-order-de84c.appspot.com/o/images-goods%2F2398557289_2f7a6b311a_z.jpg?alt=media&token=dbe07cee-c91d-4108-80bc-b681c717809c';
+const arr = [ img1, img2, img3, img4, img5, img6, img7, img8, img9, img10 ];
+
+// const arr = [ img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15 ];
 
 const CatalogIndex = props => {
   const { options, dispatch, goods, goodsGroups, prices, current, cart } = props;
@@ -40,7 +52,6 @@ const CatalogIndex = props => {
   };
   let pictContainerStyle = {display: 'flex', padding: '8px', marginTop: '6px'};
   pictContainerStyle = cartItems[currentGuid] ? { ...pictContainerStyle, border: '1px solid goldenrod'} : pictContainerStyle
-  // const url1 = "http://loremflickr.com/320/240/vegetable?random=" + current;
   const url1 = arr[parseInt(Math.random()* 10, 10) ];
   return (
     <Paper className='catalog' style={style} zDepth={2}>
